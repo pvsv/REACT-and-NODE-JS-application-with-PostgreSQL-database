081520a9bba4b9da239a4907899ed0bfa2c016cb
PAMIDIGHANTAM VENKATA SAI VEERANJANEYULU <110541286+pvsv@users.noreply.github.com>
2024-03-03 16:41:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+
+
+const express = require('express');
+const app = express();
+const dataRoutes = require('./routes/dataRoutes');
+const pool = require('./db'); // Import the database connection pool
+const cors = require("cors");
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use('/api', dataRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    pool.connect((err, client, release) => {
+        if (err) {
+            console.error('Error connecting to the PostgreSQL database:', err.message);
+        } else {
+            console.log('Connected to the PostgreSQL database');
+        }
+        release(); // release the client back to the pool
+    });
+});
